0850d9068d5a70370d3a2e28137bd698a0c73a36
lhem43 <ledominhanh@gmail.com>
2024-11-14 15:43:36 +0700

    Temp commit of tracker.py

--- tracker.py
+++ tracker.py
@@ -2,15 +2,13 @@ import socket
 from threading import Thread
 
 class Tracker:
-   def __init__(self, host, port):
-      self.host = host
+   def __init__(self, port=8000):
+      self.host = self.get_host_default_interface_ip()
       self.port = port
-      self.peers = []
-      # self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+      self.sock = socket.socket()
       self.sock.bind((self.host, self.port))
-    
-
-    # get ip address of the Tracker
+      self.sock.listen(10)
+   
    def get_host_default_interface_ip():              
       s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
       try:
@@ -21,3 +19,23 @@ class Tracker:
       finally:
          s.close()
       return ip
+   def run(self):
+      print("Listening on: {}:{}".format(self.host, self.port))
+      while True:
+         try:
+            conn, addr = self.sock.accept()
+            nconn = Thread(target=self.new_connection, args=(conn,addr))
+            nconn.start()
+         except Exception:
+            print(Exception)
+            break
+            
+            
+   def new_connection(self, conn, addr):
+      while True:
+         try:
+            receiveData = conn.recv(1024)
+            data = receiveData.decode("utf-8")
+         except Exception:
+            print(Exception)
+            break
